d7ccf5c9454e6b981831a66b14f52b257bf3a1e7
Carbuino <19510288+Carbuino@users.noreply.github.com>
2026-06-05 18:32:48 -0600

    Clear config.ts
    Reset config.ts back to default values

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,17 @@
 import { ActivityType } from 'discord.js';
-import type { BotConfig } from './structures/types'; 
+import type { BotConfig } from './structures/types';
 
 export const config: BotConfig = {
-    groupId: 5039397,
+    groupId: 0,
     slashCommands: true,
     legacyCommands: {
-        enabled: false,
+        enabled: true,
         prefixes: ['q!'],
     },
     permissions: {
-		// @Commissioner, @Program Director, @Founder, @Admin (#happyplace), @V0YD Bot
-        all: ['598772957237608458','598133902053474304','705775681715568651', '898663114847952956','853946662295699486'],
+        all: [''],
         ranking: [''],
-		// @XP permissions
-        users: ['1017876633522274445'],
+        users: [''],
         shout: [''],
         join: [''],
         signal: [''],
@@ -32,123 +30,14 @@ export const config: BotConfig = {
     recordManualActions: true,
     memberCount: {
         enabled: false,
-        channelId: '726022531483697203',
-        milestone: 1000,
-        onlyMilestones: true,
+        channelId: '',
+        milestone: 100,
+        onlyMilestones: false,
     },
     xpSystem: {
-        enabled: true,
-        autoRankup: true,
-        roles: [
-            {
-                rank: 28,
-                xp: 1000,
-            },
-            {
-                rank: 27,
-                xp: 920,
-            },
-            {
-                rank: 26,
-                xp: 830,
-            },
-            {
-                rank: 25,
-                xp: 750,
-            },
-            {
-                rank: 24,
-                xp: 680,
-            },
-            {
-                rank: 23,
-                xp: 610,
-            },
-            {
-                rank: 22,
-                xp: 550,
-            },
-            {
-                rank: 21,
-                xp: 500,
-            },
-            {
-                rank: 20,
-                xp: 450,
-            },
-            {
-                rank: 19,
-                xp: 405,
-            },
-            {
-                rank: 18,
-                xp: 360,
-            },
-            {
-                rank: 17,
-                xp: 320,
-            },
-	    {
-                rank: 16,
-                xp: 280,
-            },
-            {
-                rank: 15,
-                xp: 245,
-            },
-            {
-                rank: 14,
-                xp: 210,
-            },
-            {
-                rank: 13,
-                xp: 180,
-            },
-            {
-                rank: 12,
-                xp: 150,
-            },
-            {
-                rank: 11,
-                xp: 125,
-            },
-            {
-                rank: 10,
-                xp: 100,
-            },
-            {
-                rank: 9,
-                xp: 80,
-            },
-            {
-                rank: 8,
-                xp: 60,
-            },
-            {
-                rank: 7,
-                xp: 45,
-            },
-            {
-                rank: 6,
-                xp: 30,
-            },
-            {
-                rank: 5,
-                xp: 20,
-            },
-            {
-                rank: 4,
-                xp: 15,
-            },
-            {
-                rank: 3,
-                xp: 10,
-            },
-            {
-                rank: 2,
-                xp: 5,
-            },
-        ],
+        enabled: false,
+        autoRankup: false,
+        roles: [],
     },
     antiAbuse: {
         enabled: false,
@@ -157,10 +46,10 @@ export const config: BotConfig = {
         demotionRank: 1,
     },
     activity: {
-        enabled: true,
+        enabled: false,
         type: ActivityType.Watching,
-        value: 'the Expeditions! (2025-11-03)',
+        value: 'for commands.',
     },
     status: 'online',
-    deleteWallURLs: true,
+    deleteWallURLs: false,
 }
